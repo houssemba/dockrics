c49567fabcef623bdd7949ed5a28be40d1d9528a
Houssem BELHADJ AHMED <houssem@belhadjahmed.com>
2015-03-23 23:11:50 +0100

    feat(webapp) : add some options to logs feature

--- app/components/containerLogs/containerLogsController.js
+++ app/components/containerLogs/containerLogsController.js
@@ -1,22 +1,29 @@
 angular.module('dockrics')
-    .controller('ContainerLogsController', ['$scope', 'ContainerLogs', '$routeParams', '$http', function ($scope, ContainerLogs, $routeParams, $http) {
-        // stdout
-        ContainerLogs.get($routeParams.id, {
-            stdout: 1,
-            stderr: 0,
-            timestamps: true,
-            tail: 100
-        }).success(function (data) {
-            $scope.stdout = data;
-        });
+    .controller('ContainerLogsController', ['$scope', 'ContainerLogs', '$routeParams', '$http', function ($scope, ContainerLogs, $routeParams) {
+        $scope.timestamps = true;
+        $scope.tail = 100;
 
-        // stderr
-        ContainerLogs.get($routeParams.id, {
-            stdout: 0,
-            stderr: 1,
-            timestamps: true,
-            tail: 100
-        }).success(function (data) {
-            $scope.stderr = data;
-        });
+        $scope.getLogs = function () {
+            // stdout
+            ContainerLogs.get($routeParams.id, {
+                stdout: 1,
+                stderr: 0,
+                timestamps: $scope.timestamps,
+                tail: $scope.tail
+            }).success(function (data) {
+                $scope.stdout = data;
+            });
+
+            // stderr
+            ContainerLogs.get($routeParams.id, {
+                stdout: 0,
+                stderr: 1,
+                timestamps: $scope.timestamps,
+                tail: $scope.tail
+            }).success(function (data) {
+                $scope.stderr = data;
+            });
+        };
+
+        $scope.getLogs();
     }]);
